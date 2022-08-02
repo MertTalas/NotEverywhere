b9c430540b9ac058c98b80bb7ca62f18c8aa65cf
Mert <218CS2101@isik.edu.tr>
2022-08-02 20:35:08 +0300

    RoomNoteDataSource and UseCases(for di) implemented

---
+++ app/src/main/java/com/mert/noteverywhere/framework/RoomNoteDataSource.kt
@@ -0,0 +1,19 @@
+package com.mert.noteverywhere.framework
+
+import android.content.Context
+import com.mert.core.data.Note
+import com.mert.core.repository.NoteRepository
+import com.mert.noteverywhere.framework.db.NoteDatabase
+import com.mert.noteverywhere.framework.db.NoteEntity
+
+class RoomNoteDataSource(context: Context): NoteRepository {
+    val noteDao = NoteDatabase.getInstance(context).noteDao()
+
+    override suspend fun addNote(note: Note) = noteDao.addNoteEntity(NoteEntity.fromNote(note))
+
+    override suspend fun getNote(id: Long) = noteDao.getNoteEntity(id)?.toNote()
+
+    override suspend fun getAllNotes() = noteDao.getAllNotesEntity().map { it.toNote() }
+
+    override suspend fun removeNote(note: Note) = noteDao.removeNoteEntity(NoteEntity.fromNote(note))
+}
---
+++ app/src/main/java/com/mert/noteverywhere/framework/UseCases.kt
@@ -0,0 +1,13 @@
+package com.mert.noteverywhere.framework
+
+import com.mert.core.usecase.AddNoteUseCase
+import com.mert.core.usecase.GetAllNotesUseCase
+import com.mert.core.usecase.GetNoteUseCase
+import com.mert.core.usecase.RemoveNoteUseCase
+
+data class UseCases(
+    val addNoteUseCase: AddNoteUseCase,
+    val gelAllNotesUseCase: GetAllNotesUseCase,
+    val getNoteUseCase: GetNoteUseCase,
+    val removeNoteUseCase: RemoveNoteUseCase
+)
