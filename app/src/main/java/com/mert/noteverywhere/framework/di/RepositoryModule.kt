81e413ea79ca7eac86764ab3955740a59028f92e
Mert <218CS2101@isik.edu.tr>
2022-08-03 15:00:21 +0300

    di classes created

---
+++ app/src/main/java/com/mert/noteverywhere/framework/di/RepositoryModule.kt
@@ -0,0 +1,14 @@
+package com.mert.noteverywhere.framework.di
+
+import android.app.Application
+import com.mert.core.repository.NoteRepositoryImpl
+import com.mert.noteverywhere.framework.RoomNoteDataSource
+import dagger.Module
+import dagger.Provides
+
+@Module
+class RepositoryModule {
+
+    @Provides
+    fun providesRepository(application: Application) = NoteRepositoryImpl(RoomNoteDataSource(application))
+}
